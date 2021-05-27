5345dac866ac0be294a5ad32e34dda9dcf2b26c6
Priscilla Baah <pyfbaah@gmail.com>
2021-05-27 11:09:53 +0000

    add more codes for dataset questions

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -17,7 +17,35 @@
 
 import pickle
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+enron_data = pickle.load(open("../final_project/final_project_dataset_unix.pkl", "rb"))
 
 
+print('number of data points: ', len(enron_data))
+sample_data_point = enron_data[list(enron_data.keys())[0]]
+print('number of features: ', len(sample_data_point.keys()))
+
+# how many POIs?
+poi_names = open("../final_project/poi_names.txt", 'r')
+next(poi_names)
+next(poi_names)
+print('Number of POIs', (len(poi_names.readlines())))
+poi_names.close()
+
+# how many emails from Wesley Colwell to POI?
+print('emails from Wesley Colwell to POIs: ', enron_data['COLWELL WESLEY']['from_this_person_to_poi'])
+
+# value of stock options exercised by Jeffrey K Skilling
+print('value of stock options Jeffrey:', enron_data['SKILLING JEFFREY K']['exercised_stock_options'])
+
+# how many people have a quantified salary?
+salary_list = []
+for key in enron_data.keys():
+    salary_list.append(enron_data[key]['salary'])
+print('number of people with a quantified salary', len(salary_list) - salary_list.count('NaN'))
+
+# number of people with a know email address
+email_list = []
+for key in enron_data.keys():
+    email_list.append(enron_data[key]['email_address'])
+print('number of people with a known email', len(email_list) - email_list.count('NaN'))
 
