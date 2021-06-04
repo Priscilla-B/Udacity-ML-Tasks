ca99aa19b802c9d38b9b0d01c752858c14a18104
Priscilla Baah <pyfbaah@gmail.com>
2021-06-04 07:33:36 +0000

    add complete code for regression with outlier removal

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -42,23 +42,24 @@ cleaned_data = []
 try:
     predictions = reg.predict(ages_train)
     cleaned_data = outlierCleaner(predictions, ages_train, net_worths_train)
-    print('new slope:', reg.coef_)
+
 except NameError:
     print("your regression object doesn't exist, or isn't name reg")
     print("can't make predictions to use in identifying outliers")
 
 
-
 # only run this code if cleaned_data is returning data
 if len(cleaned_data) > 0:
     ages, net_worths, errors = zip(*cleaned_data)
-    ages = numpy.reshape( numpy.array(ages), (len(ages), 1))
-    net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
+    ages = numpy.reshape(numpy.array(ages), (len(ages), 1))
+    net_worths = numpy.reshape(numpy.array(net_worths), (len(net_worths), 1))
 
     # refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
         plt.plot(ages, reg.predict(ages), color="blue")
+        print('new slope:', reg.coef_)
+        print('new score on test data:', reg.score(ages_test, net_worths_test))
     except NameError:
         print("you don't seem to have regression imported/created,")
         print("or else your regression object isn't named reg")
