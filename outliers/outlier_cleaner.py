7e08b9eaefb3d44a947d4e064da100697e9a67ca
Priscilla Baah <pyfbaah@gmail.com>
2021-06-04 06:46:27 +0000

    update outliers files

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -2,6 +2,7 @@
 
 
 def outlierCleaner(predictions, ages, net_worths):
+
     """
         Clean away the 10% of points that have the largest
         residual errors (difference between the prediction
@@ -11,10 +12,10 @@ def outlierCleaner(predictions, ages, net_worths):
         each tuple is of the form (age, net_worth, error).
     """
     
-    cleaned_data = []
 
-    ### your code goes here
+    zipped = zip(ages, net_worths, abs(predictions - net_worths))
+    cleaned_data = list(zipped)
 
-    
-    return cleaned_data
+    cleaned_data.sort(key=lambda tup: tup[2])
 
+    return cleaned_data[:round(len(cleaned_data) * 0.9)]
